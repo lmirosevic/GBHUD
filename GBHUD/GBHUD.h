88f1343895653793c67d73f7411e5aa36e179a0a
Luka Mirosevic <luka@goonbee.com>
2012-11-21 08:18:59 +0100

    Initial Commit

---
+++ GBHUD/GBHUD.h
@@ -0,0 +1,13 @@
+//
+//  GBHUD.h
+//  GBHUD
+//
+//  Created by Luka Mirosevic on 21/11/2012.
+//  Copyright (c) 2012 Goonbee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GBHUD : NSObject
+
+@end
---
+++ GBHUD/GBHUD.m
@@ -0,0 +1,13 @@
+//
+//  GBHUD.m
+//  GBHUD
+//
+//  Created by Luka Mirosevic on 21/11/2012.
+//  Copyright (c) 2012 Goonbee. All rights reserved.
+//
+
+#import "GBHUD.h"
+
+@implementation GBHUD
+
+@end
